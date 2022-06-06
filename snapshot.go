9635a9d12bb3aec143d39fda9e501871fd945b2d
Steph Samson <hello@stephsamson.com>
2022-06-06 15:43:34 +0200

    feat: add output option to snapshot downloader

--- snapshot.go
+++ snapshot.go
@@ -25,6 +25,11 @@ var SnapshotCmd = &cli.Command{
 			Usage:   "the max number of attempts to make when finding a snapshot in the bucket",
 			Value:   4,
 		},
+		&cli.StringFlag{
+			Name:    "output-path",
+			Aliases: []string{"o"},
+			Usage:   "the file path to store the snapshot to",
+		},
 	},
 	Action: func(c *cli.Context) error {
 		if c.Args().Len() == 0 {
@@ -34,7 +39,21 @@ var SnapshotCmd = &cli.Command{
 
 		date := c.Args().Get(0)
 
-		cl := newClient(s3Host, c.Int("max-attempts"))
+		opts := []func(*client){
+			withHost(s3Host),
+			withMaxAttempts(c.Int("max-attempts")),
+		}
+
+		o := c.String("output-path")
+		if o != "" {
+			p, err := os.Create(o)
+			if err != nil {
+				return fmt.Errorf("could not create file: %s", err)
+			}
+			opts = append(opts, withOutputPath(p))
+		}
+
+		cl := newClient(opts...)
 
 		s, err := cl.getSnapshot(date, 0)
 		if err != nil {
@@ -51,10 +70,33 @@ var SnapshotCmd = &cli.Command{
 type client struct {
 	host        string
 	maxAttempts int
+	outputPath  *os.File
 }
 
-func newClient(host string, maxAttempts int) client {
-	return client{host: host, maxAttempts: maxAttempts}
+func newClient(opts ...func(*client)) *client {
+	c := &client{}
+	for _, o := range opts {
+		o(c)
+	}
+	return c
+}
+
+func withHost(host string) func(*client) {
+	return func(c *client) {
+		c.host = host
+	}
+}
+
+func withMaxAttempts(m int) func(*client) {
+	return func(c *client) {
+		c.maxAttempts = m
+	}
+}
+
+func withOutputPath(outputPath *os.File) func(*client) {
+	return func(c *client) {
+		c.outputPath = outputPath
+	}
 }
 
 func (c *client) getSnapshot(date string, attempt int) (*os.File, error) {
@@ -98,9 +140,15 @@ func (c *client) getSnapshot(date string, attempt int) (*os.File, error) {
 		return nil, fmt.Errorf("could not download car file: %s", err)
 	}
 
-	file, err := os.Create(car)
-	if err != nil {
-		return nil, fmt.Errorf("could not create file: %s", err)
+	var file *os.File
+	if c.outputPath == nil {
+		var err error
+		file, err = os.Create(car)
+		if err != nil {
+			return nil, fmt.Errorf("could not create file: %s", err)
+		}
+	} else {
+		file = c.outputPath
 	}
 
 	if _, err := io.Copy(file, resp.Body); err != nil {
