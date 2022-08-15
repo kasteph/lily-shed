b9fa8697a901c3da4b08b3c46b546608e7f3f022
Steph Samson <hello@stephsamson.com>
2022-08-16 01:18:02 +0200

    feat: add print option to snapshot. resolves #8

--- snapshot_test.go
+++ snapshot_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -29,4 +30,34 @@ func TestGetSnapshot(t *testing.T) {
 	o, _ := ioutil.ReadAll(f)
 	assert.Equal(t, []byte("snapshot"), o)
 	os.Remove("minimal_finality_stateroots_1590960_2022-02-28_08-00-00.car")
+}
+
+func TestGetSnapshotPrintToStdout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Disposition", "attachment; filename=test")
+		w.Header().Set("Accept-Ranges", "bytes")
+		//w.Write([]byte("snapshot"))
+
+		contents := []byte("snapshot")
+		http.ServeContent(w, r, "test", time.Now(), bytes.NewReader(contents))
+	}))
+	defer ts.Close()
+
+	stdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	c := newClient(withHost(ts.URL), withMaxAttempts(4), withPrint(true))
+	s, _ := c.getSnapshot("2022-02-28_08-00-00", 0)
+	w.Close()
+	os.Stdout = stdout
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+
+	assert.Nil(t, s)
+	assert.Equal(
+		t,
+		ts.URL+"/minimal_finality_stateroots_1590960_2022-02-28_08-00-00.car",
+		buf.String(),
+	)
 }
