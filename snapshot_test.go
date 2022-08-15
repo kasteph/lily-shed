bb9cbec8d61bdc0640e6c5060b647f7ce25b6252
Steph Samson <hello@stephsamson.com>
2022-08-16 01:22:07 +0200

    fix: use io over ioutil

--- snapshot_test.go
+++ snapshot_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -27,7 +26,7 @@ func TestGetSnapshot(t *testing.T) {
 	c := newClient(withHost(ts.URL), withMaxAttempts(4))
 	s, _ := c.getSnapshot("2022-02-28_08-00-00", 0)
 	f, _ := os.OpenFile(s.Name(), os.O_RDONLY, 0755)
-	o, _ := ioutil.ReadAll(f)
+	o, _ := io.ReadAll(f)
 	assert.Equal(t, []byte("snapshot"), o)
 	os.Remove("minimal_finality_stateroots_1590960_2022-02-28_08-00-00.car")
 }
